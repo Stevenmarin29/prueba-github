cc1d254c35810eaef9440a964d70fd13d9696564
steven <cmaringuevara25@gmail.com>
2024-12-12 00:21:17 -0500

    commit 2

---
+++ cadenastexto.js
@@ -0,0 +1,25 @@
+let texto1 = 'javaScript';
+let texto2 = 'javaScript';
+let texto3 ='Eso es la verdad de todo';
+let texto4 ="Eso es la 'verdad' de todo";
+
+let texto5 = 'esto es un texto demasiado largo que necesita \
+un salto de linea, pero que sucede, al colocarlo entre \
+comillas simples no se puede'
+
+let texto6 = 'esto es un texto demasiado largo que necesita'+'<br>'+ 
+'un salto de linea, pero que sucede, al colocarlo entre'+'<br>'+
+'comillas simples no se puede'
+
+
+document.write(texto1);
+document.write('<br>');
+document.write(texto2);
+document.write('<br>');
+document.write(texto3);
+document.write('<br>');
+document.write(texto4);
+document.write('<br>');
+document.write(texto5);
+document.write('<br>');
+document.write(texto6);
